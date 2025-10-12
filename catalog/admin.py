0f40d371ea3208c0a3c990c07a2491aba671ee07
Hoori-D <shahdima39@gmail.com>
2025-10-12 15:30:05 +0300

    updated Plant model, added Plant model in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Plant
+
+
+@admin.register(Plant)
+class PlantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    list_display_links = ['name']
+    list_editable = ['price']
+
+    readonly_fields = ['slug']
+
+    search_fields = ['name']
+
+    ordering = ['name']
+    sortable_by = ['name', 'price']
--- catalog/models.py
+++ catalog/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Plant(models.Model):
-    name = models.CharField(max_length=50, unique=True, blank=False, null=False, db_index=True, verbose_name='Название')
-    description = models.TextField(default='', unique=False, blank=False, null=False, verbose_name='Описание товара')
-    slug = models.SlugField(max_length=100, unique=True, blank=False, null=False, verbose_name='URL-адрес')
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, unique=False, blank=False, null=False, db_index=True ,verbose_name='Цена')
-    image = models.ImageField(upload_to='catalog', unique=False, blank=False, null=False, verbose_name='Изображение товара')
+    name = models.CharField(max_length=50, unique=True, db_index=True, verbose_name='Название')
+    description = models.TextField(default='', verbose_name='Описание товара')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='URL-адрес')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, db_index=True ,verbose_name='Цена')
+    image = models.ImageField(upload_to='catalog', verbose_name='Изображение товара')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления товара')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время обновления товара')
