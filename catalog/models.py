3a29d9697c5c2d125541d0fd3a3efc5b78903274
Hoori-D <shahdima39@gmail.com>
2025-10-12 15:07:45 +0300

    added Plant model in catalog app

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=50, unique=True, blank=False, null=False, db_index=True, verbose_name='Название')
+    description = models.TextField(default='', unique=False, blank=False, null=False, verbose_name='Описание товара')
+    slug = models.SlugField(max_length=100, unique=True, blank=False, null=False, verbose_name='URL-адрес')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, unique=False, blank=False, null=False, db_index=True ,verbose_name='Цена')
+    image = models.ImageField(upload_to='catalog', unique=False, blank=False, null=False, verbose_name='Изображение товара')
