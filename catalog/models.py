0db2235a4b656862ca0ebb3eda38f4a6deee7500
Hoori-D <shahdima39@gmail.com>
2025-10-13 14:30:26 +0300

    change directory for plant images model

--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,7 @@ class Plant(models.Model):
     description = models.TextField(default='', verbose_name='Описание товара')
     slug = models.SlugField(max_length=100, unique=True, verbose_name='URL-адрес')
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, db_index=True ,verbose_name='Цена')
-    image = models.ImageField(upload_to='catalog', verbose_name='Изображение товара')
+    image = models.ImageField(upload_to='catalog/plants', verbose_name='Изображение товара')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления товара')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время обновления товара')
 
