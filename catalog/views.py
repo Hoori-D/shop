0ce012fcfcda0e5b2e62e3b52ae6a4e943f92e85
Hoori-D <shahdima39@gmail.com>
2025-10-14 19:00:53 +0300

    added category objects in view, added html for category

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
-from catalog.models import Plant
+from catalog.models import Plant, Category
 
 
 def index(request):
     plants = Plant.objects.all()
+    categories = Category.objects.all()
     context = {
         'title': 'Каталог',
+        'categories': categories,
         'plants': plants,
     }
     return render(request, 'catalog/index.html', context=context)
