2b676e1fc63f09dac805cde647d2bccc619ce107
Hoori-D <shahdima39@gmail.com>
2025-10-25 19:11:37 +0300

    added working pagination on catalog page

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name='catalog'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('page=<int:page_number>', views.index, name='index'),
     path('category/<slug:category_slug>/', views.category_view, name='category_by_slug'),
     path('plant/<slug:plant_slug>/', views.plant_view, name='plant_by_slug'),
 ]
