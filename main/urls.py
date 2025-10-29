107f81aaa2698492042031f55feefcd84b212941
Hoori-D <shahdima39@gmail.com>
2025-10-29 14:49:23 +0300

    class view for main page

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name='main'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from catalog.models import Category
 
 
-def index(request):
-    categories = Category.objects.all()
+class IndexView(ListView):
+    template_name = 'main/index.html'
+    model = Category
+    context_object_name = 'categories'
 
-    context = {
-        'title': 'Главная страница',
-        'categories': categories,
-    }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Главная страница'
+        return context
 
-    return render(request, 'main/index.html', context)
 
 class AboutView(TemplateView):
     template_name = 'main/about.html'
