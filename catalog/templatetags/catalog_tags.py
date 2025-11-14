2a78f30a8f3971cfa7f4c97823020ed969755078
Hoori-D <shahdima39@gmail.com>
2025-11-14 17:55:52 +0300

    fix sort with pagination

--- catalog/templatetags/catalog_tags.py
+++ catalog/templatetags/catalog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.http import urlencode
 
 from catalog.models import Category
 
@@ -10,3 +11,10 @@ register = template.Library()
 def tag_categories():
     categories = Category.objects.all()
     return {'categories': categories}
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
