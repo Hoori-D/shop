6816508ceff5954fb3d07274c4a4c94b3aeb045f
Hoori-D <shahdima39@gmail.com>
2025-11-19 18:22:48 +0300

    change search

--- catalog/utils.py
+++ catalog/utils.py
@@ -1,5 +1,11 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, \
+    SearchRank
+
 from catalog.models import Plant
 
 
 def q_search(query):
-    return Plant.objects.filter(name__search=query)
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
+
+    return Plant.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
