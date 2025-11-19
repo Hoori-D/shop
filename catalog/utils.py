b3235490e158b892ab4a3f4250c9ee33b600d982
Hoori-D <shahdima39@gmail.com>
2025-11-19 18:24:36 +0300

    small change search

--- catalog/utils.py
+++ catalog/utils.py
@@ -8,4 +8,4 @@ def q_search(query):
     vector = SearchVector('name', 'description')
     query = SearchQuery(query)
 
-    return Plant.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+    return Plant.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by('-rank')
