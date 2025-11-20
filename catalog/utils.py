646f00631bcf7c5f9151631767b1ce5e1c158686
Hoori-D <shahdima39@gmail.com>
2025-11-20 19:33:03 +0300

    search headline (not working)

--- catalog/utils.py
+++ catalog/utils.py
@@ -1,5 +1,5 @@
 from django.contrib.postgres.search import SearchVector, SearchQuery, \
-    SearchRank
+    SearchRank, SearchHeadline
 
 from catalog.models import Plant
 
@@ -8,4 +8,4 @@ def q_search(query):
     vector = SearchVector('name', 'description')
     query = SearchQuery(query)
 
-    return Plant.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by('-rank')
+    return Plant.objects.annotate(headline=SearchHeadline('name', query, start_sel='<span style="background-color: yellow;"', stop_sel='</span>'))
