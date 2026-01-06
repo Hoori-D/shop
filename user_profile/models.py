5dd4f168a2fec92c0a450321f49986b5da9fb647
Hoori-D <shahdima39@gmail.com>
2026-01-06 18:00:17 +0300

    model for pforile

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='user_profile/profile_images/', verbose_name='Изображение профиля')
+
+    def __str__(self):
+        return f'Профиль пользователя {self.user.username}'
+
+@receiver(post_save,sender=get_user_model())
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save,sender=get_user_model())
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
+    else:
+        Profile.objects.create(user=instance)
+
