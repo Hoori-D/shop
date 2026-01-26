a65e10c9ce4f0845b10cc602f86d4bd02cd9c416
Hoori-D <shahdima39@gmail.com>
2026-01-26 18:02:35 +0300

    added nginx and gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "Migrations"
 uv run python manage.py migrate
 
+echo "Collect static"
+uv run manage.py collectstatic
+
 echo "Runserver"
 exec uv run python manage.py runserver 0.0.0.0:8000
 
--- shop/settings.py
+++ shop/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv("DJANGO_SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['LOCALHOST', '127.0.0.1', '0.0.0.0']
 
@@ -150,6 +150,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
 
 MEDIA_URL = '/media/'
