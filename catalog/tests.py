f04eb3a03569dfdda8828971e70c18fefb2bd175
Hoori-D <shahdima39@gmail.com>
2026-01-31 19:06:05 +0300

    category and plant model str tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -7,9 +7,11 @@ class PlantTest(TestCase):
     def test_get_absolute_url(self):
         plant = Plant(name='Test', description='Test description', slug='test', image='test.jpg', price=100)
         self.assertEqual(plant.get_absolut_url(), '/catalog/plant/test/')
+        self.assertEqual(str(plant), 'Test')
 
 
 class CategoryTest(TestCase):
     def test_get_absolute_url(self):
         category = Category(name='Test', slug='test')
         self.assertEqual(category.get_absolut_url(), '/catalog/test/')
+        self.assertEqual(str(category), 'Test')
