cf54f1dd218fccf7acc82055a949afc83030af9d
Hoori-D <shahdima39@gmail.com>
2026-01-31 19:03:33 +0300

    category model test create

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 
-from .models import Plant
+from .models import Plant, Category
 
 
 class PlantTest(TestCase):
     def test_get_absolute_url(self):
         plant = Plant(name='Test', description='Test description', slug='test', image='test.jpg', price=100)
         self.assertEqual(plant.get_absolut_url(), '/catalog/plant/test/')
+
+
+class CategoryTest(TestCase):
+    def test_get_absolute_url(self):
+        category = Category(name='Test', slug='test')
+        self.assertEqual(category.get_absolut_url(), '/catalog/test/')
