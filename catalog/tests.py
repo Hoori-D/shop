c52d8c9d10a8f1bb39111d0d37a4e1f78de112d9
Hoori-D <shahdima39@gmail.com>
2026-01-31 18:31:32 +0300

    first test

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Plant
+
+
+class PlantTest(TestCase):
+    def test_get_absolute_url(self):
+        plant = Plant(name='Test', description='Test description', slug='test', image='test.jpg', price=100)
+        self.assertEqual(plant.get_absolut_url(), '/catalog/plant/test/')
