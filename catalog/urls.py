0c958a309e6e00187db422b7c9cabda1a194a115
Hoori-D <shahdima39@gmail.com>
2026-02-01 14:45:37 +0300

    CI/CD test

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,5 @@ app_name='catalog'
 urlpatterns = [
     path('search/', views.IndexView.as_view(), name='search'),
     path('<slug:slug>/', views.IndexView.as_view(), name='category_by_slug'),
-    path('plant/<slug:slug>/', views.PlantDetailView.as_view(), name='plant_by_slug'),
+    path('plants/<slug:slug>/', views.PlantDetailView.as_view(), name='plant_by_slug'),
 ]
