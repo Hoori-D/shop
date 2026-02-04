a8047b0a11873da8bf6a5f9c505bf3daa17fa2a3
Hoori-D <shahdima39@gmail.com>
2026-02-04 16:45:12 +0300

    added test for plant model and added sale and stock to plant model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.urls import reverse
@@ -9,8 +10,9 @@ class Plant(models.Model):
     name = models.CharField(max_length=50, unique=True, db_index=True, verbose_name='Название')
     description = models.TextField(default='', verbose_name='Описание товара')
     slug = models.SlugField(max_length=100, unique=True, verbose_name='URL-адрес')
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, db_index=True ,verbose_name='Цена')
-    sale = models.DecimalField(max_digits=3, decimal_places=0, default=0,verbose_name='Скидка в процентах')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, validators=[MinValueValidator(0.00, 'Цена не может быть меньше 0')], db_index=True ,verbose_name='Цена')
+    sale = models.DecimalField(max_digits=3, decimal_places=0, default=0, validators=[MinValueValidator(0.00, 'Скидка не может быть меньше 0'), MaxValueValidator(100.00, 'Скидка не может быть больше 100')],verbose_name='Скидка в процентах')
+    stock_count = models.IntegerField(default=0, validators=[MinValueValidator(0, 'Количество товара не может быть меньше 0')], verbose_name='Количество товара')
     image = models.ImageField(upload_to='catalog/plants', verbose_name='Количество посещений')
     views_count = models.PositiveIntegerField(default=0, verbose_name='Популярность товара')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления товара')
@@ -21,9 +23,12 @@ class Plant(models.Model):
         verbose_name = 'Растение'
         verbose_name_plural = 'Растения'
 
-
     def __str__(self):
         return self.name
+
+    @property
+    def in_stock(self):
+        return self.stock_count > 0
 
     def save(self, *args, **kwargs):
         if not self.slug:
--- catalog/tests.py
+++ catalog/tests.py
@@ -1,24 +1,45 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from .models import Plant, Category
 
 
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test', slug='test')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.category.get_absolut_url(), '/catalog/test/')
+
+
+
 class PlantTest(TestCase):
     def setUp(self):
-        self.plant = Plant(name='Test', description='Test description', sale=5, slug='test', image='test.jpg', price=125)
-        self.plant2 = Plant(name='Test2', description='Test description2', sale=38, slug='test', image='test.jpg', price=372)
+        self.category = Category.objects.create(name='Test', slug='test')
+        self.plant = Plant.objects.create(name='Test', description='Test description', stock_count=20, sale=20, slug='test', image='test.jpg', price=125, category=Category.objects.get(name='Test'))
+
+    def test_negative_sale(self):
+        self.plant.sale = -100
+        with self.assertRaises(ValidationError):
+            self.plant.full_clean()
+
+    def test_gte100_sale(self):
+        self.plant.sale = 101
+        with self.assertRaises(ValidationError):
+            self.plant.full_clean()
+
+    def test_negative_in_stock(self):
+        self.plant.stock_count = -100
+        with self.assertRaises(ValidationError):
+            self.plant.full_clean()
+
+    def test_negative_price(self):
+        self.plant.price = -100
+        with self.assertRaises(ValidationError):
+            self.plant.full_clean()
 
     def test_get_absolute_url(self):
         self.assertEqual(self.plant.get_absolut_url(), '/catalog/plant/test/')
 
     def test_get_sale_price(self):
-        self.assertEqual(self.plant.get_sale_price(), 118.75)
-        self.assertEqual(self.plant2.get_sale_price(), 230.64)
-
-
-class CategoryTest(TestCase):
-    def setUp(self):
-        self.category = Category(name='Test', slug='test')
-
-    def test_get_absolute_url(self):
-        self.assertEqual(self.category.get_absolut_url(), '/catalog/test/')
+        self.assertEqual(self.plant.get_sale_price(), 100.00)
