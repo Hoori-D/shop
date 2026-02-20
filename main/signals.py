44c2dd755a3eef0fc9427b0d3c9b995ccc4ee273
Hoori-D <shahdima39@gmail.com>
2026-02-20 12:10:18 +0300

    small change in signals

--- main/signals.py
+++ main/signals.py
@@ -3,10 +3,6 @@ from django.contrib.auth.signals import user_logged_out, user_logged_in
 from django.core.cache import cache
 
 
-@receiver(user_logged_out)
-def clear_main_page_on_logout(sender, request, user, **kwargs):
-    cache.delete_pattern('*main_page*')
-
-@receiver(user_logged_in)
+@receiver([user_logged_in, user_logged_out])
 def clear_main_page_on_logout(sender, request, user, **kwargs):
     cache.delete_pattern('*main_page*')
