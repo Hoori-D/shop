4083508a42b4d68c12b54c04898530d3510c8d25
Hoori-D <shahdima39@gmail.com>
2026-02-23 12:58:05 +0300

    change url to REST

--- carts/urls.py
+++ carts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name='carts'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('add_item/<slug:slug>', views.add_item, name='add_item'),
-    path('change_item/<slug:slug>', views.change_item, name='change_item'),
-    path('remove_item/<slug:slug>', views.remove_item, name='remove_item'),
+    path('<int:pk>/', views.IndexView.as_view(), name='index'),
+    path('add_item/<slug:slug>/', views.add_item, name='add_item'),
+    path('change_item/<slug:slug>/', views.change_item, name='change_item'),
+    path('remove_item/<slug:slug>/', views.remove_item, name='remove_item'),
 ]
